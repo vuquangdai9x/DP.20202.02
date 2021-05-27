1b6c60f3d619c75686f2d49666fea4fa94ac5152
Vu Quang Dai <vuquangdai9x@gmail.com>
2021-05-27 17:05:49 +0700

    + add adapter to calculate delivery info by new method

---
+++ case_study_week_2/src/main/java/entity/shipping/AltDeliveryInfoAdapter.java
@@ -0,0 +1,58 @@
+package entity.shipping;
+
+import entity.order.Order;
+
+import org.example.AlternativeDistanceCalculator;
+
+public class AltDeliveryInfoAdapter extends DeliveryInfo {
+	protected float width;
+    protected float height;
+    protected float depth;
+    protected float weight;
+	protected AlternativeDistanceCalculator altDistanceCalculator;
+	
+	public AltDeliveryInfoAdapter(String name, String phone, String province, String address,
+			String shippingInstructions, AlternativeDistanceCalculator altDistanceCalculator) {
+		super(name, phone, province, address, shippingInstructions, null);
+		this.altDistanceCalculator = altDistanceCalculator;
+	}
+	
+	@Override
+	public int calculateShippingFee(Order order) {
+		// use alt calculator
+        int distance = altDistanceCalculator.calculateDistance(address);
+        return (int) (distance * 1.2);
+    }
+
+	public float getWidth() {
+		return width;
+	}
+
+	public void setWidth(float width) {
+		this.width = width;
+	}
+
+	public float getHeight() {
+		return height;
+	}
+
+	public void setHeight(float height) {
+		this.height = height;
+	}
+
+	public float getDepth() {
+		return depth;
+	}
+
+	public void setDepth(float depth) {
+		this.depth = depth;
+	}
+
+	public float getWeight() {
+		return weight;
+	}
+
+	public void setWeight(float weight) {
+		this.weight = weight;
+	}
+}
